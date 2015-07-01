3f271979aba1f4f628b268eca9a8b0dda11cfcc1
Jan Kaluza <jkaluza@redhat.com>
2015-07-01 10:22:59 +0200

    Fix tests with localhost2

--- 017/requests.sh
+++ 017/requests.sh
@@ -1,9 +1,9 @@
 source ../functions.sh
 
-handled_by_8080 "localhost" "https://localhost:9443/owncloud"
-handled_by_8080 "localhost" "https://localhost:9443/owncloud/"
-handled_by_8080 "localhost" "https://localhost:9443/owncloud/another"
+handled_by_8080_ssl "localhost" "https://localhost:9443/owncloud"
+handled_by_8080_ssl "localhost" "https://localhost:9443/owncloud/"
+handled_by_8080_ssl "localhost" "https://localhost:9443/owncloud/another"
 
-handled_by_8081 "localhost2" "https://localhost:9443/owncloud"
-handled_by_8081 "localhost2" "https://localhost:9443/owncloud/"
-handled_by_8081 "localhost2" "https://localhost:9443/owncloud/another"
+handled_by_8081_ssl "localhost2" "https://localhost:9443/owncloud"
+handled_by_8081_ssl "localhost2" "https://localhost:9443/owncloud/"
+handled_by_8081_ssl "localhost2" "https://localhost:9443/owncloud/another"
